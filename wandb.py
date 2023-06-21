42a681083d4d014e1471f2b212e67ce3be43eaa1
Hadi-Mohseni <74792164+Hadi-Mohseni@users.noreply.github.com>
2023-06-21 15:29:56 +0330

    feat: added wandb.py

---
+++ wandb.py
@@ -0,0 +1,47 @@
+from torch.nn import Module
+from wandb.sdk.wandb_run import Run
+import os
+import torch
+import wandb
+
+
+def load_model(artifact: str, model: Module, version: str, run: Run):
+    """
+    load saved checkpoint of a model
+
+    Parameters
+    ----------
+    artifact : str
+        name of the artifact the model saved on.
+    model : Module
+        the model which the parameters should be loaded.
+    version : str
+        version of the artifact. e.g. "latest"
+    run : Run
+        wandb Run that uses artifact.
+
+    Returns
+    -------
+    (run, model, epoch)
+        - run: wandb Run that uses the artifact from now on.
+        - model: pretrained model.
+        - epoch: resuming epoch number.
+    """
+    try:
+        artifact = run.use_artifact(f"{model}:{version}", type="model")
+        artifact_dir = artifact.download()
+        if os.path.exists(artifact_dir):
+            checkpoint = torch.load(os.path.join(artifact_dir, "checkpoint.pt"))
+            model.load_state_dict(checkpoint["encoder"])
+            epoch = checkpoint["epoch"]
+    except:
+        epoch = 0
+
+    return run, model, epoch
+
+
+def save_model(run: Run, params: dict, artifact: str):
+    torch.save(params, "checkpoint.pt")
+    save_artifact = wandb.Artifact(name=artifact, type="model")
+    save_artifact.add_file("checkpoint.pt")
+    run.log_artifact(save_artifact)
