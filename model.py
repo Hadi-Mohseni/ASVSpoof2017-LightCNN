9c8536ffd8dbec9c16b9c07647384597efda67b7
Hadi Mohseni <hadimohseni1996@gmail.com>
2023-06-25 17:41:08 +0330

    refactor: model.py is modified
    - model.py coding is better and compacted
    - model.py is documented

--- model.py
+++ model.py
@@ -3,16 +3,36 @@ import torch
 
 
 class mfm(nn.Module):
-    def __init__(self, out_channels: int, type=1):
+    """
+    MFM activation function that merges channels to
+    keep the model light. Simply divides channels into
+    two groups and then selects the maximum.
+
+    Paper: A light CNN for deep face representation with noisy labels
+    """
+
+    def __init__(self, out_channels: int, type: int = 1) -> None:
+        """
+        Parameters
+        ----------
+        out_channels : int
+            number of channels in the output
+        type : int, optional
+            type=1 --> two group of channels and uses maximum `out.size=(B, out_channels/2, H, W)`
+            type=2 --> three groups, uses max and mean `out.size=(B, 2*out_channels/3, H, W)`
+            by default 1
+        """
         super(mfm, self).__init__()
         self.out_channels = out_channels
         self.type = type
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         if self.type == 1:
+            # split channels into two groups
             x = torch.split(x, self.out_channels, 1)
             out = torch.max(x[0], x[1])
-        else:
+        if self.type == 2:
+            # split channels into three groups
             x = torch.split(x, int(self.out_channels / 2), 1)
             out1 = torch.max(x[0], x[1])
             out2 = (x[1] + x[2]) / 2
@@ -21,108 +41,96 @@ class mfm(nn.Module):
         return out
 
 
-class LightCNN(nn.Module):
-    def __init__(self, in_channels: int, initializer=nn.init.xavier_normal_) -> None:
-        super(LightCNN, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, 32, 5, 1, padding=2)
-        self.mfm1 = mfm(16)
-        initializer(self.conv1.weight)
+class NinBlock(nn.Module):
+    """
+    Network in network block
+    ---------
 
-        self.conv2a = nn.Conv2d(16, 32, 1, 1)
-        self.mfm2a = mfm(16)
-        self.conv2b = nn.Conv2d(16, 48, 3, 1, padding=1)
-        self.mfm2b = mfm(24)
-        initializer(self.conv2a.weight)
-        initializer(self.conv2b.weight)
+    Network in network block adds a mapping like mlp
+    (here a convolution is used) between each two pair of
+    convolution layers.
 
-        self.conv3a = nn.Conv2d(24, 48, 1, 1)
-        self.mfm3a = mfm(32, type=2)
-        self.conv3b = nn.Conv2d(32, 64, 3, 1, padding=1)
-        self.mfm3b = mfm(32)
-        initializer(self.conv3a.weight)
-        initializer(self.conv3b.weight)
+    Paper: Network in network
+    """
 
-        self.conv4a = nn.Conv2d(32, 64, 1, 1)
-        self.mfm4a = mfm(32)
-        self.conv4b = nn.Conv2d(32, 32, 3, 1, padding=1)
-        self.mfm4b = mfm(16)
-        initializer(self.conv4b.weight)
-        initializer(self.conv4a.weight)
-
-        self.conv5a = nn.Conv2d(16, 32, 1, 1)
-        self.mfm5a = mfm(16)
-        self.conv5b = nn.Conv2d(16, 32, 3, 1, padding=1)
-        self.mfm5b = mfm(16)
-        initializer(self.conv5b.weight)
-        initializer(self.conv5a.weight)
-
-        self.flat = nn.Flatten()
-
-        self.fc6 = nn.Linear(5184, 64)
-        self.mfm6 = mfm(32)
-
-        self.fc7 = nn.Linear(32, 2)
-
+    def __init__(self, in_channels: int, out_channels: int) -> None:
+        """
+        Parameters
+        ----------
+        in_channels : int
+            number of input channels
+        out_channels : int
+            number of expected channels as output
+        """
+        super(NinBlock, self).__init__()
+        self.nin = nn.Conv2d(in_channels, 2 * in_channels, 1, 1)
+        self.mfm1 = mfm(in_channels)
+        self.bn1 = nn.BatchNorm2d(in_channels)
+        self.conv = nn.Conv2d(in_channels, out_channels * 2, 3, 1, padding=1)
+        self.mfm2 = mfm(out_channels)
         self.maxpool = nn.MaxPool2d(2, 2)
-        self.dropout = nn.Dropout(p=0.7)
-        self.softmax = nn.Softmax(dim=1)
-        self.bn1 = nn.BatchNorm2d(16)
-        self.bn2 = nn.BatchNorm2d(16)
-        self.bn3 = nn.BatchNorm2d(24)
-        self.bn4 = nn.BatchNorm2d(24)
-        self.bn5 = nn.BatchNorm2d(32)
-        self.bn6 = nn.BatchNorm2d(32)
-        self.bn7 = nn.BatchNorm2d(16)
-        self.bn8 = nn.BatchNorm2d(16)
-        self.bn9 = nn.BatchNorm2d(16)
+        self.bn2 = nn.BatchNorm2d(out_channels)
 
-    def forward(self, x: torch.Tensor):
-        x = self.conv1(x)
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.nin(x)
         x = self.mfm1(x)
-        x = self.maxpool(x)
         x = self.bn1(x)
-
-        x = self.conv2a(x)
-        x = self.mfm2a(x)
-        x = self.bn2(x)
-
-        x = self.conv2b(x)
-        x = self.mfm2b(x)
+        x = self.conv(x)
+        x = self.mfm2(x)
         x = self.maxpool(x)
-        x = self.bn3(x)
+        out = self.bn2(x)
 
-        x = self.conv3a(x)
-        x = self.mfm3a(x)
-        x = self.bn4(x)
+        return out
 
-        x = self.conv3b(x)
-        x = self.mfm3b(x)
-        x = self.maxpool(x)
-        x = self.bn5(x)
 
-        x = self.conv4a(x)
-        x = self.mfm4a(x)
-        x = self.bn6(x)
+class LightCNN(nn.Module):
+    """
+    LightCNN
+    ----------
 
-        x = self.conv4b(x)
-        x = self.mfm4b(x)
-        x = self.maxpool(x)
-        x = self.bn7(x)
+    An implementaion of LightCNN used for ASVSpoof2017
+    Replay attack. note that this model is being fed with
+    spectrogram.
 
-        x = self.conv5a(x)
-        x = self.mfm5a(x)
-        x = self.bn8(x)
+    Paper: Audio replay attack detection with deep learning frameworks
+    """
 
-        x = self.conv5b(x)
-        x = self.mfm5b(x)
-        x = self.maxpool(x)
-        x = self.bn9(x)
+    def __init__(self, in_channels: int = 1) -> None:
+        """
+        Parameters
+        ----------
+        in_channels : int
+            number of input channels.
+        """
+        super(LightCNN, self).__init__()
 
-        x = self.flat(x)
-        x = self.fc6(x)
-        x = self.mfm6(x)
+        self.block1 = nn.Sequential(
+            nn.Conv2d(in_channels, 32, 5, 1, padding=2),
+            mfm(16),
+            nn.MaxPool2d(2, 2),
+            nn.BatchNorm2d(16),
+        )
 
-        x = self.dropout(x)
-        x = self.fc7(x)
-        x = self.softmax(x)
-        return x
+        self.backbone = nn.Sequential(
+            NinBlock(16, 24),
+            NinBlock(24, 32),
+            NinBlock(32, 16),
+            NinBlock(16, 16),
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(5184, 64),
+            mfm(32),
+            nn.Linear(32, 2),
+            nn.Dropout(p=0.7),
+        )
+
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.block1(x)
+        x = self.backbone(x)
+        x = self.classifier(x)
+        pred = self.softmax(x)
+        return pred
